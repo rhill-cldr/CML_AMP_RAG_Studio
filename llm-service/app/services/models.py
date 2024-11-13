3125c3adba173d49fbef2580be742198abc1a609
Michael Liu <mliu@cloudera.com>
2024-11-13 11:28:39 -0800

    Start implementing embedding_model_test

--- llm-service/app/services/models.py
+++ llm-service/app/services/models.py
@@ -37,16 +37,18 @@
 #
 import os
 from enum import Enum
+from typing import Literal
 
 from fastapi import HTTPException
 from llama_index.core.base.embeddings.base import BaseEmbedding
 from llama_index.core.llms import LLM
 from llama_index.embeddings.bedrock import BedrockEmbedding
 from llama_index.llms.bedrock import Bedrock
-from .llama_utils import messages_to_prompt, completion_to_prompt
 
+from .caii import get_caii_embedding_models, get_caii_llm_models
 from .caii import get_embedding_model as caii_embedding
-from .caii import get_llm as caii_llm, get_caii_embedding_models, get_caii_llm_models
+from .caii import get_llm as caii_llm
+from .llama_utils import completion_to_prompt, messages_to_prompt
 
 
 def get_embedding_model() -> BaseEmbedding:
@@ -55,13 +57,14 @@ def get_embedding_model() -> BaseEmbedding:
     return BedrockEmbedding(model_name="cohere.embed-english-v3")
 
 
-def get_llm(model_name: str=None) -> LLM:
+def get_llm(model_name: str = None) -> LLM:
     if "CAII_DOMAIN" in os.environ:
-        return caii_llm(domain=os.environ["CAII_DOMAIN"],
-                        endpoint_name=os.environ["CAII_INFERENCE_ENDPOINT_NAME"],
-                        messages_to_prompt=messages_to_prompt,
-                        completion_to_prompt=completion_to_prompt,
-                        )
+        return caii_llm(
+            domain=os.environ["CAII_DOMAIN"],
+            endpoint_name=os.environ["CAII_INFERENCE_ENDPOINT_NAME"],
+            messages_to_prompt=messages_to_prompt,
+            completion_to_prompt=completion_to_prompt,
+        )
     return Bedrock(
         model=model_name,
         context_size=128000,
@@ -69,21 +72,25 @@ def get_llm(model_name: str=None) -> LLM:
         completion_to_prompt=completion_to_prompt,
     )
 
+
 def get_available_embedding_models():
     if "CAII_DOMAIN" in os.environ:
         return get_caii_embedding_models()
     return _get_bedrock_embedding_models()
+
 
 def get_available_llm_models():
     if "CAII_DOMAIN" in os.environ:
         return get_caii_llm_models()
     return _get_bedrock_llm_models()
 
+
 def _get_bedrock_llm_models():
-    return [{
-        "model_id": "Llama31-8bInstructV1",
-        "name": "Llama3.1 8B Instruct v1",
-    },
+    return [
+        {
+            "model_id": "Llama31-8bInstructV1",
+            "name": "Llama3.1 8B Instruct v1",
+        },
         {
             "model_id": "Llama31-70bInstructV1",
             "name": "Llama3.1 70B Instruct v1",
@@ -94,33 +101,52 @@ def _get_bedrock_llm_models():
         },
     ]
 
+
 def _get_bedrock_embedding_models():
-    return [{
-        "model_id": "cohere.embed-english-v3",
-        "name": "cohere.embed-english-v3",
-    }]
+    return [
+        {
+            "model_id": "cohere.embed-english-v3",
+            "name": "cohere.embed-english-v3",
+        }
+    ]
+
 
 class ModelSource(str, Enum):
     BEDROCK = "Bedrock"
     CAII = "CAII"
+
 
 def get_model_source() -> ModelSource:
     if "CAII_DOMAIN" in os.environ:
         return ModelSource.CAII
     return ModelSource.BEDROCK
 
-def test_llm_model(model_name: str) -> str:
+
+def test_llm_model(model_name: str) -> Literal["ok"]:
     models = get_available_llm_models()
     for model in models:
         if model["model_id"] == model_name:
             if model["available"]:
                 get_llm(model_name).complete("Are you available to answer questions?")
-                return 'ok'
+                return "ok"
             else:
                 raise HTTPException(status_code=503, detail="Model unavailable")
 
     raise HTTPException(status_code=404, detail="Model not found")
 
-def test_embedding_model() -> str:
-    get_embedding_model()
-    return 'ok'
+
+def test_embedding_model(model_name: str) -> Literal["ok"]:
+    models = get_available_embedding_models()
+    for model in models:
+        if model["model_id"] == model_name:
+            print(f"{model=}")  # TODO: remove this after we're done debugging
+            if model["available"]:  # TODO: KeyError here!!!
+                # NOTE: when we make embedding models configurable, pass the name to get_embedding_model()
+                get_embedding_model().get_text_embedding("Embed this")
+                return "ok"
+            else:
+                raise HTTPException(status_code=503, detail="Model unavailable")
+
+    raise HTTPException(status_code=404, detail="Model not found")
+
+    return "ok"
