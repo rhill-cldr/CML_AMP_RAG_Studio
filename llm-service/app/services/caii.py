ecc14bf3ea64f180dee506ed6e1af943e4e241d7
jwatson <jkwatson@gmail.com>
2024-11-15 08:03:31 -0800

    check the name lower-case

--- llm-service/app/services/caii.py
+++ llm-service/app/services/caii.py
@@ -77,7 +77,7 @@ def get_llm(domain: str, endpoint_name: str, messages_to_prompt, completion_to_p
 
     model = endpoint["endpointmetadata"]["model_name"]
     print(f"Model: {model}")
-    if "mistral" in endpoint_name:
+    if "mistral" in endpoint_name.lower():
         print("using customized mistral model")
         llm = CaiiModelMistral(
             model=model,
